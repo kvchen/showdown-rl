16078c020d41e353cf8cd482cd1aec2214b5cece
Kevin Chen <kvchen@fb.com>
2018-12-06 10:37:59 -0800

    Fork ppo

--- main.py
+++ main.py
@@ -6,23 +6,7 @@ import random
 import tensorflow as tf
 from functools import partial
 from gym_showdown.envs import ShowdownEnv
-from spinup.utils.run_utils import ExperimentGrid
-from spinup import ppo
-
-
-def random_battle(env):
-    num_actions = len(env.ALL_ACTIONS)
-
-    is_terminal = False
-    while not is_terminal:
-        _state, _reward, is_terminal = env.step(
-            [random.randint(0, num_actions - 1), random.randint(0, num_actions - 1)]
-        )
-
-    num_moves = len(env.current_battle["data"]["inputLog"])
-    print(f"Terminated in {num_moves} moves")
-
-    print(env.current_battle["data"])
+from ppo.ppo import ppo
 
 
 @click.command()
@@ -32,13 +16,12 @@ def random_battle(env):
     default="gen1randombattle",
     help="The format of the battle to use.",
 )
-@click.option("--cpu", type=int, default=4)
-def main(format: str, cpu: int):
+def main(format: str, *args, **kwargs):
     env_fn = partial(ShowdownEnv, {"formatid": format})
     ac_kwargs = dict(hidden_sizes=(64, 64))
 
     with tf.Graph().as_default():
-        ppo(env_fn, epochs=100, ac_kwargs=ac_kwargs)
+        ppo(env_fn, ac_kwargs=ac_kwargs)
 
 
 if __name__ == "__main__":
